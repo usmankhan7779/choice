f2f2bedf3f4a942cde5d2455bffbc1d4b8c56e36
usmankhanbrainplow <usman.khanbrainplow.com>
2018-03-23 18:35:14 +0500

    aa

--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -9,12 +9,7 @@ export class HomeService {
   constructor(private http: Http) { }
 
   searchProducts(id,page) {
-<<<<<<< HEAD
-      return this.http.get(Config.api+'data_against_zipcode/'+id+'?page='+page).map((response: Response) => response.json());
-     // return this.http.get('http://192.168.30.52:9000/choice/zipcodedata/'+id+'?page='+page).map((response: Response) => response.json());
-=======
      // return this.http.get(Config.api+'data_against_zipcode/'+id+'?page='+page).map((response: Response) => response.json());
       return this.http.get('http://192.168.30.52:9000/choice/zipcodedata/'+id+'?page='+page).map((response: Response) => response.json());
->>>>>>> 15623c51ef3d13ae30e2731e1b5df9fc073ba7fc
   }
 }
